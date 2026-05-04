4778669f46fd4aebebfde880cbbeb97d883be9b6
SRINIKHILMASIMUKKU <140952559+SRINIKHILMASIMUKKU@users.noreply.github.com>
2026-05-04 15:02:30 +0530

    Implement a decorator to enhance function output
    Added a decorator function to wrap another function with pre- and post-execution print statements.

--- decoratorwithout@.py
+++ decoratorwithout@.py
@@ -1,1 +1,11 @@
+def my_deco(func):
+    def wrapper():
+        print("Before function")
+        func()
+        print("After function")
+    return wrapper
 
+def say_hello():
+    print("Hello")
+decorated = my_deco(say_hello)
+decorated()
